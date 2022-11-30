da3d6de3e3072359088393a7994aef24755dd59b
Georgi Yanakiev <gyanakiev@appolica.com>
2022-11-30 06:53:53 +0200

    Added padding manager, font manager, app's font, cicrle avatar on home screen

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FontFamily {
+  static const String openSansFontFamily = 'Urbanist';
+}
+
+class FontWeightManager {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static const double s4 = 4.0;
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../presentation/padding_manager.dart';
+import 'home_viewmodel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('1914'),
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<HomeVM>();
+    return Scaffold(
+      body: ColoredBox(
+        color: Colors.red,
+        child: SafeArea(
+          child: ColoredBox(
+            color: Theme.of(context).backgroundColor,
+            child: Column(
+              children: [
+                Padding(
+                  padding: PaddingManager.p1,
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          //todo Fetch the image from db
+                          'https://img.favpng.com/7/7/21/pfc-levski-sofia-bc-levski-sofia-pfc-lokomotiv-plovdiv-first-professional-football-league-png-favpng-LqV2xLbsMkmrGgwi99kMSxf3D.jpg',
+                        ),
+                        minRadius: 24,
+                      ),
+                      Column(
+                        children: const [
+                          //todo Fetch from db
+                          Text('Good morning'),
+                          Text('Name SecondName'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Container(
-          color: Theme.of(context).backgroundColor,
-        ),
-      );
+      ),
+    );
+  }
 }
