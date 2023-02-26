d5d72a6986628bcec5e41b9f84dbb5c6863c462f
Georgi Yanakiev <gyanakiev@appolica.com>
2023-02-26 12:09:54 +0200

    Extracted bottom navigation bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,34 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../../presentation/color_manager.dart';
+import 'components/home_nav_bar.dart';
 import 'components/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          unselectedLabelStyle: TextStyle(color: AppColors.white),
-          unselectedItemColor: AppColors.white,
-          backgroundColor: AppColors.backgroundColor,
-          selectedItemColor: AppColors.primaryPurple,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined), label: 'Home'),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite_outline,
-              ),
-              label: 'Favorites',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: 'Profile',
-            )
-          ],
-        ),
+  Widget build(BuildContext context) => const Scaffold(
+        bottomNavigationBar: HomeNavBar(),
         backgroundColor: AppColors.backgroundColor,
-        body: const HomeScreenBody(),
+        body: HomeScreenBody(),
       );
 }
