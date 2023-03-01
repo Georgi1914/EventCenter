ed120d193f64d1e705c9c9518e898e09783b697b
Georgi Yanakiev <gyanakiev@appolica.com>
2023-03-01 19:43:56 +0200

    Extracted widgets

---
+++ lib/screens/description_screen/components/description_body.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import '../../../global_variables.dart';
+import '../../../models/ui/event.dart';
+import 'description_text_forms.dart';
+
+class DescriptionBody extends StatelessWidget {
+  const DescriptionBody({
+    required this.event,
+    super.key,
+  });
+
+  final DomainEvent event;
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Image.network(
+                imageUrls[Random().nextInt(imageUrls.length)],
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            DescriptionTextForms(
+              event: event,
+            ),
+          ],
+        ),
+      );
+}
--- lib/screens/description_screen/description_view.dart
+++ lib/screens/description_screen/description_view.dart
@@ -1,12 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../global_variables.dart';
 import '../../models/ui/event.dart';
-import '../custom_widgets/icon_text_row.dart';
-import 'description_view_model.dart';
+import 'components/description_body.dart';
 
 class DescriptionScreen extends StatelessWidget {
   const DescriptionScreen({
@@ -17,51 +12,21 @@ class DescriptionScreen extends StatelessWidget {
   final DomainEvent event;
 
   @override
-  Widget build(BuildContext context) {
-    final viewModel = context.watch<DescriptionVM>();
-    final theme = Theme.of(context).textTheme;
-
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           elevation: 0,
           leading: const BackButton(),
-          actions: [Icon(Icons.favorite)],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(Icons.favorite_outline),
+              ),
+            )
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Image.network(
-                  imageUrls[Random().nextInt(imageUrls.length)],
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      event.name,
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    IconTextRow(
-                      icon: Icons.calendar_month,
-                      text: viewModel.formattedDate(event.date),
-                    ),
-                    IconTextRow(icon: Icons.location_pin, text: event.address),
-                    IconTextRow(
-                      icon: Icons.description_outlined,
-                      text: event.description,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
+        body: DescriptionBody(event: event),
+      );
 }
